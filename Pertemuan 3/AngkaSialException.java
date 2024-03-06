3a3e0ea038c6027f5fe5ed106231785161a821b3
FikriAzka <142158263+FikriAzka@users.noreply.github.com>
2024-03-06 16:57:48 +0700

    Add files via upload

---
+++ Pertemuan 3/AngkaSialException.java
@@ -0,0 +1,7 @@
+// Nama         : Fikri Azka Pradya
+// NIM          : 24060122140171
+public class AngkaSialException extends Exception {
+    public AngkaSialException(){
+        super("Jangan memasukkan angka 13 karena angka sial !!!");
+    }
+}
