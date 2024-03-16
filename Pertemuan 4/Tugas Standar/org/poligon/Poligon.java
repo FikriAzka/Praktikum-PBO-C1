b4462bbf88fb1755a4bbb0a7ac1b58d85f8c973c
FikriAzka <142158263+FikriAzka@users.noreply.github.com>
2024-03-16 23:46:15 +0700

    Add files via upload

---
+++ Pertemuan 4/Tugas Standar/org/poligon/Poligon.java
@@ -0,0 +1,14 @@
+// Nama         : Fikri Azka Pradya
+// NIM          : 24060122140171
+// Deskripsi    : representasi dasar dari objek poligon (segi banyak)
+// Tanggal      : 16 Maret 2024 
+
+package org.poligon;
+
+public class Poligon {
+    protected int jumlahSisi;
+
+    public int getJumlahSisi(){
+        return this.jumlahSisi;
+    }
+}
