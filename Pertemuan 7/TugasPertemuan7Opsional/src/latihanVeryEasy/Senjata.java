29a7b260d845dde86242ea914102e12cc8ffd845
FikriAzka <142158263+FikriAzka@users.noreply.github.com>
2024-05-05 00:30:55 +0700

    Add files via upload

---
+++ Pertemuan 7/TugasPertemuan7Opsional/src/latihanVeryEasy/Senjata.java
@@ -0,0 +1,44 @@
+// Nama     : Fikri Azka Pradya
+// NIM      : 24060122140171
+// Tanggal  : 5 Mei 2024
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package latihanVeryEasy;
+
+/**
+ *
+ * @author Azka
+ */
+public class Senjata {
+    private String bunyi;
+    private int peluru;
+
+    public Senjata(String bunyi) {
+        this.bunyi = bunyi;
+        this.peluru = 0;
+    }
+
+    public String getBunyi(){
+        return bunyi;
+    }
+
+    public int getPeluru(){
+        return peluru;
+    }
+    
+    public void menembak(){
+        System.out.println(getBunyi());
+        System.out.println("Sisa Peluru: " + (getPeluru() - 1));
+    }
+
+    public void setBunyi(String bunyi) {
+        this.bunyi = bunyi;
+    }
+
+    public void setPeluru(int peluru){
+        this.peluru = peluru;
+    }
+}
